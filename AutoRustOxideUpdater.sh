9c426e4b07ca32ca4df22babcbeae39d5b1f3635
zybeon <yates.rodney@gmail.com>
2020-02-03 17:01:19 -0500

    Added use of webrcon
    Added use of webrcon and now requires the pre-requisite of webrcon, install with 'npm i webrconjs'
    Used webrcon to send notice to players that a server update is available and will restart within a specified amount of time.
    Use webrcon to properly stop the server with rcon 'quit' command

--- AutoRustOxideUpdater.sh
+++ AutoRustOxideUpdater.sh
@@ -1,36 +1,59 @@
 #!/bin/bash
+#############
+## Prerequisites
+#############
+## https://www.npmjs.com/package/webrcon
+## npm i webrconjs
+## 
+#############
+
+### Server RCON variables
+serverip=127.0.0.1 #ip of game server
+serverport=28016
+serverrconpass=ChangeME 
+
 #########
 # START #
 #########
 echo "Starting . . ."
 echo "Downloading Latest Oxide version info"
-curl https://umod.org/games/rust/latest.json | jq .version > latestOxide.txt
+curl -s https://umod.org/games/rust/latest.json | jq .version > latestOxide.txt
+
 if [ -f installedOxide.txt ]
 then
-  echo "installed.txt already exists, checking for update"
+  echo "installedOxide.txt already exists, checking for update"
 else
   touch installedOxide.txt
-  echo "installed does not exist, created empty installed.txt"
+  echo "installedOxide.txt does not exist, created empty installedOxide.txt"
 fi
 
 if cmp -s installedOxide.txt latestOxide.txt
 then 
  echo "No update required"
 else
- echo "Starting Update, make sure your server is turned off before continuing!"
+ echo "New update is available!"
 #######################
-#Run Server Update here
+#Run Server Update here using webrcon.
 #######################
+node rustrconsend.js "say Server restart in 3 minutes!!\nRust Server Update is available.\nUpdate your client before reconnecting." ${serverip} ${serverport} ${rconpass}
+sleep 1m
+node rustrconsend.js "say Server restarting in 2 minute!!\nRust Server Update is available.\nUpdate your client before reconnecting." ${serverip} ${serverport} ${rconpass}
+sleep 1m
+node rustrconsend.js "say Server restarting in 1 minute!!\nRust Server Update is available.\nUpdate your client before reconnecting." ${serverip} ${serverport} ${rconpass}
+sleep 30s
+node rustrconsend.js "say Server restarting in 30 seconds!!\nRust Server Update is available.\nUpdate your client before reconnecting." ${serverip} ${serverport} ${rconpass}
+sleep 25s
+node rustrconsend.js "say Server restarting in 5 seconds!!\nRust Server Update is available.\nUpdate your client before reconnecting." ${serverip} ${serverport} ${rconpass}
+sleep 5s
+node rustrconsend.js "quit" ${serverip} ${serverport} ${rconpass}
 
-#Run command to inform player there is a new client update
-# and to exit to install. Later incorporate checking for
-# first Thursday of the month to notify players server is
-# wiping. include wiping all on that day 'rustserver wa'
+# Later incorporate checking for first Thursday of the month to notify players server is wiping. include wiping all on that day 'rustserver wa'
+
 bash ~/rustserver sp
 bash ~/rustserver u
 bash ~/rustserver ma
 bash ~/rustserver st
-echo "Rust, Oxide and installed.json has been updated"
+echo "Rust, Oxide and installedOxide.txt has been updated"
 cp latestOxide.txt installedOxide.txt
 fi
 
