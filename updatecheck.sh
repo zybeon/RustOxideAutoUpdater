378ce05ed7568d9c46169eb1b4e67e30227bfaac
zybeon <yates.rodney@gmail.com>
2020-02-04 16:43:57 -0500

    Added process check
    using PID file to check if the script is running and exit if it is.
    Used a for loop to compress script and increase notice to 10 second countdown.

--- updatecheck.sh
+++ updatecheck.sh
@@ -6,15 +6,46 @@
 ## npm i webrconjs
 #############
 
+## Script install location
+dir=/opt/RustOxideAutoUpdater
+
+### Checking if script is already running
+PIDFILE=$dir/updatecheck.pid
+if [ -f $PIDFILE ]
+then
+  PID=$(cat $PIDFILE)
+  ps -p $PID > /dev/null 2>&1
+  if [ $? -eq 0 ]
+  then
+    echo "Job is already running"
+    exit 1
+  else
+    ## Process not found assume not running
+    echo $$ > $PIDFILE
+    if [ $? -ne 0 ]
+    then
+      echo "Could not create PID file"
+      exit 1
+    fi
+  fi
+else
+  echo $$ > $PIDFILE
+  if [ $? -ne 0 ]
+  then
+    echo "Could not create PID file"
+    exit 1
+  fi
+fi
+
 ### Server RCON variables
 serverip=0.0.0.0 #ip of game server
 serverport=28016
 rconpass=ChangeME
 
 ### Variables
-rustsend=/opt/RustOxideAutoUpdater/rustrconsend.js
-installed=/opt/RustOxideAutoUpdater/installedOxide.txt
-latest=/opt/RustOxideAutoUpdater/latestOxide.txt
+rustsend=$dir/rustrconsend.js
+installed=$dir/installedOxide.txt
+latest=$dir/latestOxide.txt
 rustserver=~/rustserver ##Location of your lgsm rustserver script
 
 #########
@@ -49,20 +80,17 @@ sleep 1m
 node $rustsend "say Server restarting in 1 minute!! Rust Server Update is available." ${serverip} ${serverport} ${rconpass}
 sleep 30s
 node $rustsend "say Server restarting in 30 seconds!! Rust Server Update is available." ${serverip} ${serverport} ${rconpass}
-sleep 24s
-node $rustsend "say Server restarting in 5 seconds!! Rust Server Update is available." ${serverip} ${serverport} ${rconpass}
-sleep 1s
-node $rustsend "say Server restarting in 4 seconds!! Rust Server Update is available." ${serverip} ${serverport} ${rconpass}
-sleep 1s
-node $rustsend "say Server restarting in 3 seconds!! Rust Server Update is available." ${serverip} ${serverport} ${rconpass}
-sleep 1s
-node $rustsend "say Server restarting in 2 seconds!! Rust Server Update is available." ${serverip} ${serverport} ${rconpass}
-sleep 1s
-node $rustsend "say Server restarting in 1 second!! Rust Server Update is available. Update your client before reconnecting." ${serverip} ${serverport} ${rconpass}
+sleep 20s
+for ((i=10; i>=1; i--))
+do
+  node $rustsend "say Server restarting in $i seconds!!" ${serverip} ${serverport} ${rconpass}
+  sleep 1s
+done
 node $rustsend "quit" ${serverip} ${serverport} ${rconpass}
-echo "######## Waiting for server to stop gracefully"
+## Waiting for server to save and start to shutdown, if conditional could be added to skip this step if it is a forced wipe day
+echo "######## Waiting 30s for server to stop gracefully"
 sleep 30s
-# Later incorporate checking for first Thursday of the month to notify players server is wiping. include wiping all on that day 'rustserver wa'
+# Later incorporate checking for first Thursday of the month to notify players server is force wiping. include wiping all command on that day 'rustserver wa'
 
 echo "######## Shuting down server ########"
 bash $rustserver sp
@@ -75,4 +103,5 @@ echo "######## Rust, Oxide and installedOxide.txt has been updated"
 cp $latest $installed
 fi
 
-exit
+rm $PIDFILE
+exit 0
