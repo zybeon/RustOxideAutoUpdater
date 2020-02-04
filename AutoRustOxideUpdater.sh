2fdc4574b21d26bd7891d60b4b741377a92c6c11
zybeon <yates.rodney@gmail.com>
2020-02-04 01:59:39 -0500

    Added variables to files, more verbose notices
    Corrected a few spelling issues
    Added variables to files to allow calling this script globally
    more verbose output
    
    Tested and working so far, we will see on Thursday for forced wipe

--- AutoRustOxideUpdater.sh
+++ AutoRustOxideUpdater.sh
@@ -4,57 +4,73 @@
 #############
 ## https://www.npmjs.com/package/webrcon
 ## npm i webrconjs
-## 
 #############
 
 ### Server RCON variables
-serverip=127.0.0.1 #ip of game server
+serverip=0.0.0.0 #ip of game server
 serverport=28016
-serverrconpass=ChangeME 
+rconpass=ChangeME
+
+### Variables
+rustsend=/opt/RustOxideUpdater/rustrconsend.js
+installed=/opt/RustOxideUpdater/installedOxide.txt
+latest=/opt/RustOxideUpdater/latestOxide.txt
 
 #########
 # START #
 #########
 echo "Starting . . ."
 echo "Downloading Latest Oxide version info"
-curl -s https://umod.org/games/rust/latest.json | jq .version > latestOxide.txt
+curl -s https://umod.org/games/rust/latest.json | jq .version > $latest
 
-if [ -f installedOxide.txt ]
+if [ -f $installed ]
 then
   echo "installedOxide.txt already exists, checking for update"
 else
-  touch installedOxide.txt
+  touch $installed
   echo "installedOxide.txt does not exist, created empty installedOxide.txt"
 fi
 
-if cmp -s installedOxide.txt latestOxide.txt
-then 
+if cmp -s $installed $latest
+then
  echo "No update required"
 else
  echo "New update is available!"
 #######################
 #Run Server Update here using webrcon.
 #######################
-node rustrconsend.js "say Server restart in 3 minutes!!\nRust Server Update is available.\nUpdate your client before reconnecting." ${serverip} ${serverport} ${rconpass}
+node $rustsend "say Server restart in 3 minutes!! Rust Server Update is available. Update your Client before reconnecting" ${serverip} ${serverport} ${rconpass}
+echo "Sleeping for 3m"
 sleep 1m
-node rustrconsend.js "say Server restarting in 2 minute!!\nRust Server Update is available.\nUpdate your client before reconnecting." ${serverip} ${serverport} ${rconpass}
+node $rustsend "say Server restarting in 2 minutes!! Rust Server Update is available." ${serverip} ${serverport} ${rconpass}
 sleep 1m
-node rustrconsend.js "say Server restarting in 1 minute!!\nRust Server Update is available.\nUpdate your client before reconnecting." ${serverip} ${serverport} ${rconpass}
+node $rustsend "say Server restarting in 1 minute!! Rust Server Update is available." ${serverip} ${serverport} ${rconpass}
 sleep 30s
-node rustrconsend.js "say Server restarting in 30 seconds!!\nRust Server Update is available.\nUpdate your client before reconnecting." ${serverip} ${serverport} ${rconpass}
-sleep 25s
-node rustrconsend.js "say Server restarting in 5 seconds!!\nRust Server Update is available.\nUpdate your client before reconnecting." ${serverip} ${serverport} ${rconpass}
-sleep 5s
-node rustrconsend.js "quit" ${serverip} ${serverport} ${rconpass}
-
+node $rustsend "say Server restarting in 30 seconds!! Rust Server Update is available." ${serverip} ${serverport} ${rconpass}
+sleep 24s
+node $rustsend "say Server restarting in 5 seconds!! Rust Server Update is available." ${serverip} ${serverport} ${rconpass}
+sleep 1s
+node $rustsend "say Server restarting in 4 seconds!! Rust Server Update is available." ${serverip} ${serverport} ${rconpass}
+sleep 1s
+node $rustsend "say Server restarting in 3 seconds!! Rust Server Update is available." ${serverip} ${serverport} ${rconpass}
+sleep 1s
+node $rustsend "say Server restarting in 2 seconds!! Rust Server Update is available." ${serverip} ${serverport} ${rconpass}
+sleep 1s
+node $rustsend "say Server restarting in 1 second!! Rust Server Update is available. Update your client before reconnecting." ${serverip} ${serverport} ${rconpass}
+node $rustsend "quit" ${serverip} ${serverport} ${rconpass}
+echo "######## Waiting for server to stop gracefully"
+sleep 30s
 # Later incorporate checking for first Thursday of the month to notify players server is wiping. include wiping all on that day 'rustserver wa'
 
+echo "######## Shuting down server ########"
 bash ~/rustserver sp
+echo "######## Updating server ############"
 bash ~/rustserver u
-bash ~/rustserver ma
+bash ~/rustserver mu
+echo "######## Starting Server ############"
 bash ~/rustserver st
-echo "Rust, Oxide and installedOxide.txt has been updated"
-cp latestOxide.txt installedOxide.txt
+echo "######## Rust, Oxide and installedOxide.txt has been updated"
+cp $latest $installed
 fi
 
 exit
